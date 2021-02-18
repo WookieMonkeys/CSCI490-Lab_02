c5b8ad8f654780d856c59b16bcdd8368f155f709
WookieMonkeys <mattwalter99@gmail.com>
2021-02-18 18:17:05 -0500

    part 1 done

--- app/src/main/java/com/example/intentdemo/MainActivity.java
+++ app/src/main/java/com/example/intentdemo/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.intentdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private Intent i;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.simpleEditText);
+
+        button = findViewById(R.id.button);
+        i = new Intent( this,SecondActivity.class );
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = editText.getText().toString();
+                i.putExtra("testString",s);
+
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/intentdemo/SecondActivity.java
+++ app/src/main/java/com/example/intentdemo/SecondActivity.java
@@ -1,6 +1,25 @@
 package com.example.intentdemo;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
+
+    TextView textView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent i = getIntent();
+        String s = i.getExtras().getString("testString");
+
+        textView2 = findViewById(R.id.textView2);
+
+        textView2.setText(s);
+    }
 }
